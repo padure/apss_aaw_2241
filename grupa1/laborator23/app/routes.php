960f42ae1727358f10046adf5fd2cf83f4b54a45
padure <paduregheorghe7@gmail.com>
2025-11-20 08:46:48 +0200

    Store si Edit

--- grupa1/laborator23/app/routes.php
+++ grupa1/laborator23/app/routes.php
@@ -6,8 +6,12 @@ use App\Controllers\CarController;
 $app->get("/", [HomeController::class, 'index']);
 //Cars
 $app->get("/cars", [CarController::class, 'index']);
+$app->get("/cars/{car}", [CarController::class, 'show']);
+//Creare
 $app->post("/cars", [CarController::class, 'store']);
 $app->get("/cars/create", [CarController::class, 'create']);
+//Editare
 $app->get("/cars/{car}/edit", [CarController::class, 'edit']);
-$app->get("/cars/{car}", [CarController::class, 'show']);
 $app->put("/cars/{car}", [CarController::class, 'update']);
+//Stergere
+$app->delete("/cars/{car}", [CarController::class, 'delete']);
